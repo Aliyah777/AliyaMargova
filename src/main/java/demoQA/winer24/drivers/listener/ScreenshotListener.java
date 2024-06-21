267b81fe5bbe7bf5db78e7409ba6e157e3e8ca51
Aliyah <zhadrayeva.aliyah@gmail.com>
2024-06-21 16:20:59 +0500

    Second Aliya's commite

--- src/main/java/demoQA/winer24/drivers/listener/ScreenshotListener.java
+++ src/main/java/demoQA/winer24/drivers/listener/ScreenshotListener.java
@@ -1,5 +1,4 @@
 package demoQA.winer24.drivers.listener;
-
 import demoQA.winer24.drivers.drivers.DriverManager;
 import io.qameta.allure.Attachment;
 import org.openqa.selenium.OutputType;
@@ -9,45 +8,43 @@ import org.testng.ITestContext;
 import org.testng.ITestListener;
 import org.testng.ITestResult;
 
-    public class ScreenshotListener implements ITestListener {
+public class ScreenshotListener implements ITestListener {
 
+    @Override
+    public void onTestFailure(ITestResult result) {
         WebDriver driver = DriverManager.getDriver();
-
-        @Override
-        public void onTestFailure(ITestResult result) {
-            if (driver != null){
-                saveScreenshotPNG();
-            }
+        if (driver != null) {
+            saveScreenshotPNG(driver);
         }
+    }
 
-        @Attachment(value = "Page screenshot", type = "image/png")
-        public byte[] saveScreenshotPNG() {
-            return ((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES);
-        }
+    @Attachment(value = "Page screenshot", type = "image/png")
+    public byte[] saveScreenshotPNG(WebDriver driver) {
+        return ((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES);
+    }
 
-        @Override
-        public void onStart(ITestContext context) {
-            System.out.println("Hello OnStart");
-        }
+    @Override
+    public void onStart(ITestContext context) {
+        System.out.println("Hello OnStart");
+    }
 
-        @Override
-        public void onTestStart(ITestResult result) {
-            System.out.println("Hello on TestStart");
-        }
+    @Override
+    public void onTestStart(ITestResult result) {
+        System.out.println("Hello on TestStart");
+    }
 
-        @Override
-        public void onTestSuccess(ITestResult result) {
-            System.out.println("Hello onTestSuccess");
-        }
+    @Override
+    public void onTestSuccess(ITestResult result) {
+        System.out.println("Hello onTestSuccess");
+    }
 
-        @Override
-        public void onFinish(ITestContext context) {
-            ITestListener.super.onFinish(context);
-        }
+    @Override
+    public void onFinish(ITestContext context) {
+        System.out.println("Hello onFinish");
+    }
 
-        @Override
-        public void onTestSkipped(ITestResult result) {
-            ITestListener.super.onTestSkipped(result);
-        }
-
+    @Override
+    public void onTestSkipped(ITestResult result) {
+        System.out.println("Hello onTestSkipped");
+    }
 }
